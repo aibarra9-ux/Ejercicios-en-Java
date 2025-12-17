3022c18918e1d103e8280334c9f25d8c773cc1bc
aibarra9-ux <alantapon26@gmail.com>
2025-12-16 22:03:59 -0600

    Añadiendo el ejercicio de idCard

---
+++ Variables/idCard.java
@@ -0,0 +1,26 @@
+public class idCard {
+    //Cree las siguientes variables:
+    /*
+    String name variable
+    String state variable
+    Character gender variable
+    Integer age variable
+    Boolean organDonor variable
+    Luego, imprima estas variables mediante println
+    */
+
+     public static void main(String[] args) {
+        // Write your code here 💖
+        String name = "Alan";
+        String state = "Muy bien";
+        char gender = 'm';
+        int edad = 18;
+        boolean organDonor = true;
+
+        System.out.println(name); 
+        System.out.println(state); 
+        System.out.println(gender); 
+        System.out.println(edad); 
+        System.out.println(organDonor); 
+    }
+}
