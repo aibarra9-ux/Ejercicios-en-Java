15704453c51ca4155f96467bcedf848b33c5ae66
aibarra9-ux <alantapon26@gmail.com>
2025-12-17 21:38:01 -0700

    Añadiendo el ejercicio de escape room

---
+++ Variables/Escape_room.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Escape_room {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hablo sin boca y oigo sin oídos. No tengo cuerpo, pero cobro vida con el viento. ¿Qué soy?");
+        String respuesta = scanner.nextLine();
+        System.out.println(respuesta);
+        System.out.println("Felicidades, es la respuesta correcta");
+    }
+}
