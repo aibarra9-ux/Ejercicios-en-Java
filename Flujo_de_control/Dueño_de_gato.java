65e1fa884b8afa1ebfaf3bf1b0877e3769c8253a
aibarra9-ux <alantapon26@gmail.com>
2025-12-18 22:11:36 -0700

    Añadiendo la carpeta de flujo_de_control y los ejercicios: Avena y Dueño de gato

---
+++ Flujo_de_control/Dueño_de_gato.java
@@ -0,0 +1,17 @@
+
+
+public class Dueño_de_gato {
+    public static void main(String[] args) {
+        // Write your code here 💖
+        boolean tieneGato = false;
+
+        if(tieneGato)
+        {
+            System.out.println("20% de descuento en artículos seleccionados para gatos con el código MEOW2025");
+        }
+        else
+        {
+            System.out.println("¡Bienvenido a la tienda Pets Pets Pets!");
+        }
+    }
+}
